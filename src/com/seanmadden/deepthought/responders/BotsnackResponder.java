07ed951cab556432b49e4963a2f57de552a552ac
sean <sean@seanmadden.net>
2010-07-23 18:06:46 -0400

    Added the botsnack responder.  //TODO replace this when we go to the factoid system

---
+++ src/com/seanmadden/deepthought/responders/BotsnackResponder.java
@@ -0,0 +1,32 @@
+/*
+ * BotsnackResponder.java
+ * 
+ *  $Id $
+ *  Author: smadden
+ *
+ *    Copyright (C) 2010 Sean Madden
+ *
+ *    Please see the pertinent documents for licensing information.
+ *
+ */
+
+package com.seanmadden.deepthought.responders;
+
+import com.seanmadden.deepthought.IRCClient;
+import com.seanmadden.deepthought.Message;
+import com.seanmadden.deepthought.MessageHandler;
+
+public class BotsnackResponder implements MessageHandler {
+
+	@Override
+	public boolean handleMessage(IRCClient irc, Message m) {
+		String message = m.getMessage();
+		if(!message.toLowerCase().contains("botsnack")){
+			return false;
+		}
+		Message msg = new Message("", "PRIVMSG", ":D", m.getTarget());
+		irc.sendMessage(msg);
+		return true;
+	}
+
+}
